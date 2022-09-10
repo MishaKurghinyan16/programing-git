84661edff08011e7a22d2ea5b2044799f381d8eb
mishakurghinyan <misha.kurghinyan.g@tumo.org>
2022-09-10 15:50:01 +0400

    Hehe

--- LivingCreature.js
+++ LivingCreature.js
@@ -1,5 +1,5 @@
 class LivingCreature {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.multiplay = 0;
@@ -14,5 +14,20 @@ class LivingCreature {
             [this.x, this.y + 1],
             [this.x + 1, this.y + 1]
         ];
+
+    }
+
+    chooseCell(char) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == char) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
     }
 }
